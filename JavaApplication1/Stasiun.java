732ea21de2d328f4c6685f063118ca8a13efa450
ariefrachmatr <ariefrachmat9696@gmail.com>
2016-03-14 21:25:54 +0700

    Added files via upload

---
+++ JavaApplication1/Stasiun.java
@@ -0,0 +1,44 @@
+public class Stasiun {
+	private String nama;
+	private String alamat;
+	private String daerah_operasi;
+
+	public Stasiun(String nama, String alamat, String daerah_operasi) {
+		this.nama = nama;
+		this.alamat = alamat;
+		this.daerah_operasi = daerah_operasi;
+	}
+
+	public void setNama(String nama) {
+		this.nama = nama;
+	}
+
+	public String getNama() {
+		return nama;
+	}
+
+	public void setAlamat(String alamat) {
+		this.alamat = alamat;
+	}
+
+	public String getAlamat() {
+		return alamat;
+	}
+
+	public void setDaerah_Operasi(String daerah_operasi) {
+		this.daerah_operasi = daerah_operasi;
+	}
+
+	public String getDaerah_Operasi() {
+		return daerah_operasi;
+	}
+
+	public void display() {
+		System.out.println("======= Info Stasiun =======");
+		System.out.println();
+		System.out.println("Nama Stasiun   : "+getNama());
+		System.out.println("Alamat Stasiun : "+getAlamat());
+		System.out.println("Daerah Operasi : "+getDaerah_Operasi());
+		System.out.println();
+	}
+}

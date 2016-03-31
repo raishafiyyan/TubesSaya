d357437ce4113286c123bdf30b6f3c97d9978c44
raishafiyyan <hafiyyanrais@gmail.com>
2016-04-01 00:40:21 +0700

    Update JavaApplication1.java

--- JavaApplication1/src/javaapplication1/JavaApplication1.java
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -15,26 +15,7 @@ public class JavaApplication1 {
      * @param args the command line arguments
      */
 	public static void main(String[] args) {
-		Stasiun s1 = new Stasiun("Kiaracondong","Bandung, Kiaracondong","Bandung");
-		Stasiun s2 = new Stasiun("Gambir","Jakarta Pusat","Jakarta");
-		Kereta k = new Kereta("Argo Parahyangan","Bandung Jakarta","Eks");
-		Gerbong g = new Gerbong("Eks01","Eks",7);
-		Rute r = new Rute();
-		Tiket t = new Tiket("T01","Eks","14 Maret 2016",50000,"Argo Parahyangan");
-
-		r.addStasiun(s1);
-		r.addStasiun(s2);
-		t.addRute(r);
-
-		k.addGerbong(g);
-		r.addKereta(k);
-
-		/*s1.display();
-		s2.display();
-		k.display();
-		r.display();*/
-		t.display();
-}
-
-    
+		Aplikasi a = new Aplikasi();
+	       a.mainMenu();
+	}
 }
